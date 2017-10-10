4da635f7fd1dc48e2bbc0e24d8d6494fdea6bbb0
Travel <ayazkara@gmail.com>
2017-10-10 15:50:34 +0300

    Gallery upload

--- traveladm/gallery.php
+++ traveladm/gallery.php
@@ -6,6 +6,23 @@
     <h2>Galeri</h2>
 
   </header>
+  <div class="row">
+							<div class="col-xs-12">
+								<section class="panel">
+									<header class="panel-heading">
+										<div class="panel-actions">
+											<a href="#" class="fa fa-caret-down"></a>
+											<a href="#" class="fa fa-times"></a>
+										</div>
+
+										<h2 class="panel-title">Galeri Upload</h2>
+									</header>
+									<div class="panel-body">
+										<form action="/upload" class="dropzone dz-square" id="dropzone-example"></form>
+									</div>
+								</section>
+							</div>
+						</div>
 <?php
  include("footer.php");
 ?>
