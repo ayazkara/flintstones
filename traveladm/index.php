87c50ecd2b3645d38ff92a1411db7b95b5149783
Travel <ayazkara@gmail.com>
2017-10-10 14:19:17 +0300

    Admin

---
+++ traveladm/index.php
@@ -0,0 +1,15 @@
+<?php
+ include("header.php");
+?>
+
+				<section role="main" class="content-body">
+					<header class="page-header">
+						<h2>Anasayfa</h2>
+
+					</header>
+
+					<!-- start: page -->
+					<!-- end: page -->
+					<?php
+					 include("footer.php");
+					?>
---
+++ traveladm/turlar.php
@@ -0,0 +1,11 @@
+<?php
+ include("header.php");
+?>
+<section role="main" class="content-body">
+  <header class="page-header">
+    <h2>Turlar</h2>
+
+  </header>
+<?php
+ include("footer.php");
+?>
